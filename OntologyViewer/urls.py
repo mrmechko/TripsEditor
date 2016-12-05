b1e19a375d5c8cc5fb0f317bffa2116ade14dd16
Rik Bose <rik@buoy.org>
2016-12-05 15:50:14 -0500

    removed unnecessary json extension

--- OntologyViewer/urls.py
+++ OntologyViewer/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^json/ontology/$', views.json_type, name='json_type'),
-    url(r'^json/word/$', views.json_get_word, name='json_get_word')
+    url(r'^ontology/$', views.json_type, name='json_type'),
+    url(r'^word/$', views.json_get_word, name='json_get_word')
 ]
