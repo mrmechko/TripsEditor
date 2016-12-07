82a45db606479304cb8971acb13c02b193d98cc5
Rik Bose <rik@buoy.org>
2016-12-06 19:54:24 -0500

    displays basic ontology info

--- client/js/OntologyViewer/script.js
+++ client/js/OntologyViewer/script.js
@@ -1,1 +1,26 @@
-alert('hello world');
+var vm = new Vue({
+  el: '#onttype',
+  data: {
+    name: 'root',
+    node: ''
+  },
+  watch: {
+    name: function(newNode) {
+      return this.getOntType();
+    }
+  },
+  methods: {
+    getOntType: _.debounce(
+      function() {
+        var vm = this;
+        axios.get('http://localhost:8000/ont/ontology/?node='+this.name)
+          .then(function (response){
+            console.log(response.data.name);
+            vm.node = response.data;
+          })
+          .catch(function (error) {
+            vm.node = error
+          })
+      }, 500)
+  }
+})
